bd18f05960e9a1065ae269d05ed4e03a6e8c8257
Eugene Teo <eugeneteoyujie@gmail.com>
2020-05-09 12:09:51 +0800

    weather service API working

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,5 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { WeatherService } from './../weather.service';
 import { Component, OnInit } from '@angular/core';
+import { ConvertPropertyBindingResult } from '@angular/compiler/src/compiler_util/expression_converter';
 
 @Component({
   selector: 'app-city',
@@ -16,9 +18,14 @@ export class CityComponent implements OnInit {
     this.service = service;
   }
   showWeather(){
-    console.log("Hello");
-    this.title = "Hello";
-    this.service.getWeather("Singapore").subscribe(res => this.text = res.base);
+    this.service.getWeather(this.title).subscribe(res => {
+      this.weatherResult = res;
+      this.text = String(this.weatherResult["main"]["temp"] - 273.15);
+      this.text += "\xB0C"
+    });
+  }
+  editCity(){
+    this.title = "Singapore";
   }
   
   ngOnInit(): void {
@@ -26,5 +33,49 @@ export class CityComponent implements OnInit {
 }
 
 export interface WeatherResult{
-  base : string;
+  coord : CoordResult,
+  weather : Weather[],
+  base : string,
+  main : MainResult,
+  visibility : number,
+  wind : WindResult,
+  clouds : CloudResult,
+  dt : number,
+  sys : SysResult
+  timezone : number,
+  id : number,
+  name : string,
+  cod : number
+}
+export interface CoordResult{
+  lon : number,
+  lat : number
+}
+export interface Weather{
+  id : number,
+  main : string,
+  description : string,
+  icon : string
+}
+export interface MainResult{
+  temp : number,
+  feels_like : number,
+  temp_min : number,
+  temp_max : number,
+  pressure : number,
+  humidity : number
+}
+export interface WindResult{
+  speed : number,
+  deg : number
+}
+export interface CloudResult{
+  all : number
+}
+export interface SysResult{
+  type : number,
+  id : number,
+  country : string,
+  sunrise : number,
+  sunset : number
 }
